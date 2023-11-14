2c3a58756e0ddb64fecf93e42f178bdcf94ce878
Morné le Roux <mornelerx@gmail.com>
2023-11-14 21:00:14 +0200

    added addTank() to TanksProvider

---
+++ lib/provider/tanks_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import '../models/tank.dart';
+
+class TanksProvider extends ChangeNotifier {
+  final List<Tank> _tanks = [];
+
+//getter for list of tanks currently added
+  List<Tank> get tanks => _tanks;
+
+//adds a tank to the list
+  addTank(
+      {required id, required tankName, required tankSize, required occupants}) {
+    _tanks.add(Tank(
+        id: id, tankName: tankName, tankSize: tankSize, occupants: occupants));
+    notifyListeners();
+  }
+}
