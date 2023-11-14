5ecd3c1fef11c7e9940336a0965c6364791d2645
Morné le Roux <mornelerx@gmail.com>
2023-11-14 20:02:05 +0200

    added params to Tank

--- lib/models/tank.dart
+++ lib/models/tank.dart
@@ -1,9 +1,13 @@
 class Tank {
+  final int id;
   final String tankName;
   final int tankSize;
+  final List<String> occupants;
 
   Tank({
+    required this.id,
     required this.tankName,
     required this.tankSize,
+    required this.occupants,
   });
 }
