57b17412b1533803cb258d5880cb64f48f1f25e1
Morné le Roux <mornelerx@gmail.com>
2023-11-15 20:02:12 +0200

    added group for multiple tests

--- test/tank_provider_test.dart
+++ test/tank_provider_test.dart
@@ -2,16 +2,18 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fish_man/provider/tank_provider.dart';
 
 void main() {
-  test(
-    "A tank should be added to your list of tanks",
-    () async {
-      //Arrange
-      var tanksProvider = TankProvider();
-      //Act
-      tanksProvider.addTank(
-          id: 23, tankName: "TestTank", tankSize: 15, occupants: ["fish"]);
-      //Assert
-      expect(tanksProvider.tanks.isNotEmpty, true);
-    },
-  );
+  group('TanksProvider Tests', () {
+    test(
+      "A tank should be added to your list of tanks",
+      () {
+        //Arrange
+        var tanksProvider = TankProvider();
+        //Act
+        tanksProvider.addTank(
+            id: 23, tankName: "TestTank", tankSize: 15, occupants: ["fish"]);
+        //Assert
+        expect(tanksProvider.tanks.isNotEmpty, true);
+      },
+    );
+  });
 }
