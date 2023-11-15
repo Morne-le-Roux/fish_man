b17ecf0d69d5d7a315e93d9e37a73326d705f934
Morné le Roux <mornelerx@gmail.com>
2023-11-15 20:36:15 +0200

    flutter init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:fish_man/presentation/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import './data/supabase_settings.dart';
 
-void main() {
+Future<void> main() async {
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
   runApp(const MyApp());
 }
 
