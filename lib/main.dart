c8a26927ccfe4e6c2472c608814d7613210021f0
Morné le Roux <mornelerx@gmail.com>
2023-11-19 19:37:21 +0200

    update supabase settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:fish_man/presentation/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import './data/supabase_settings.dart';
+import './core/supabase_settings.dart';
 
 Future<void> main() async {
   await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
