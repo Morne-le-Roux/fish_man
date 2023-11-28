89068cc4451a8acf74a4a99d24a36895de9e12fa
Morné le Roux <mornelerx@gmail.com>
2023-11-28 21:47:28 +0200

    now displays tanks

--- lib/features/tanks_list/presentation/screens/tanks_list_screen.dart
+++ lib/features/tanks_list/presentation/screens/tanks_list_screen.dart
@@ -1,23 +1,26 @@
 import 'package:fish_man/features/tanks_list/data/supabase_retrieve.dart';
+import 'package:fish_man/features/tanks_list/presentation/widgets/tank_widget.dart';
+import 'package:fish_man/features/tanks_list/provider/tank_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class TanksListScreen extends StatelessWidget {
+  const TanksListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: InkWell(
-          onTap: () async {
-            SupabaseRetrieve supabaseRetrieve = SupabaseRetrieve();
-            print(await supabaseRetrieve.getListOfTanks());
+      body: ChangeNotifierProvider(
+        create: (context) => TankProvider(),
+        child: Consumer<TankProvider>(
+          builder: (context, tankProvider, child) {
+            return ListView.builder(
+              itemCount: tankProvider.listOfTanks.length,
+              itemBuilder: (BuildContext context, int index) {
+                return TankWidget(tank: tankProvider.listOfTanks[index]);
+              },
+            );
           },
-          child: Container(
-            color: Colors.blue,
-            height: 100,
-            width: 100,
-          ),
         ),
       ),
     );
