44e0e0c0b910d2db3cab4c3fb531866a810436fb
Morné le Roux <mornelerx@gmail.com>
2023-11-28 21:47:39 +0200

    edited tank widget

--- lib/features/tanks_list/presentation/widgets/tank_widget.dart
+++ lib/features/tanks_list/presentation/widgets/tank_widget.dart
@@ -11,9 +11,23 @@ class TankWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      elevation: 5,
-      child: Container(),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        elevation: 10,
+        color: Colors.blueGrey.shade400,
+        child: Container(
+          padding: const EdgeInsets.all(12),
+          height: 200,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(tank.tankName),
+              Text("${tank.tankSize} Gallon"),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
