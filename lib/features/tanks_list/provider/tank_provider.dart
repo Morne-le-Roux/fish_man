42b04856e0b048715b4bb845e7ab4291753bffce
Morné le Roux <mornelerx@gmail.com>
2023-11-28 21:47:50 +0200

    now.... works.

--- lib/features/tanks_list/provider/tank_provider.dart
+++ lib/features/tanks_list/provider/tank_provider.dart
@@ -4,10 +4,28 @@ import '../models/tank.dart';
 
 class TankProvider extends ChangeNotifier {
 //List of tanks
-  final List<Tank> _listOfTanks = SupabaseRetrieve().getListOfTanks();
+  final List<Tank> _listOfTanks = [];
+  TankProvider() {
+    getListOfTanks();
+  }
 
 //getter for list of tanks currently added
   List<Tank> get listOfTanks => _listOfTanks;
+
+//get tanks list
+
+  getListOfTanks() async {
+    var data = await SupabaseRetrieve().getListOfTanks();
+    print(data);
+    for (var tank in data) {
+      _listOfTanks.add(Tank(
+          id: tank["id"],
+          tankName: tank["tank_name"],
+          tankSize: tank["tank_size"],
+          occupants: tank["occupants"]));
+    }
+    notifyListeners();
+  }
 
 //adds a tank to the list
   addTank(
