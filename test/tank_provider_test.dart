597eed1d19856284da855158baf3fa531c536d2c
Morné le Roux <mornelerx@gmail.com>
2023-11-30 18:18:44 +0200

    removed unused code

--- test/tank_provider_test.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:fish_man/features/tanks_list/provider/tank_provider.dart';
-
-void main() {
-  group('TanksProvider Tests', () {});
-}
