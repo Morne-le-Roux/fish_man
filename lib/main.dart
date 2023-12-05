d078dad717ee277d6fd794d450d9229a56fd27e3
Morné le Roux <mornelerx@gmail.com>
2023-12-05 21:43:11 +0200

    open register screen on app open

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:fish_man/core/constants.dart';
+import 'package:fish_man/features/auth/presentation/screens/register_screen.dart';
 import 'package:fish_man/features/tanks_list/presentation/screens/tanks_list_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const TanksListScreen(),
+      home: const RegisterScreen(),
       darkTheme: darkTheme,
       theme: lightTheme,
     );
