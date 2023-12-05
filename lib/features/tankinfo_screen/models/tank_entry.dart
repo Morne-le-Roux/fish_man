bb9e1295af55de4208d560dc9b9d921845ec6e72
Morné le Roux <mornelerx@gmail.com>
2023-12-05 20:30:29 +0200

    removed print statement

--- lib/features/tankinfo_screen/models/tank_entry.dart
+++ lib/features/tankinfo_screen/models/tank_entry.dart
@@ -1,7 +1,7 @@
 class TankEntry {
   final Map _data;
 
-  TankEntry({required Map<dynamic, dynamic> data}) : _data = data {
-    print(_data);
-  }
+  TankEntry({required Map<dynamic, dynamic> data}) : _data = data;
+
+  Map get data => _data;
 }
