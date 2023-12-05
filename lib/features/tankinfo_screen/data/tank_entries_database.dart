5ad8cfd01c2e5e38442a01b68b9afe833c513b3b
Morné le Roux <mornelerx@gmail.com>
2023-12-05 20:29:14 +0200

    fixed wrong queries

--- lib/features/tankinfo_screen/data/tank_entries_database.dart
+++ lib/features/tankinfo_screen/data/tank_entries_database.dart
@@ -1,13 +1,11 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class TankEntriesDatabase {
-  final supabase = Supabase.instance.client;
+  final _supabase = Supabase.instance.client;
 
   getListOfTankEntries(tankID) async {
-    final data = await supabase
-        .from("tankEntries")
-        .select("tankID")
-        .eq("tankID", tankID);
+    final data = await _supabase.from("tankEntries").select().eq("id", tankID);
+
     return data;
   }
 }
