0dec9d2073bf5eb32ad7dafd132757bb8b83722a
Morné le Roux <mornelerx@gmail.com>
2023-12-06 19:04:10 +0200

    init AuthTextBox

--- lib/features/auth/presentation/widgets/auth_text_box.dart
+++ lib/features/auth/presentation/widgets/auth_text_box.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
 
 class AuthTextBox extends StatelessWidget {
-  const AuthTextBox({super.key});
+  const AuthTextBox({
+    super.key,
+    required this.hint,
+    required this.controller,
+    required this.icon,
+  });
+
+  final String hint;
+  final TextEditingController controller;
+  final IconData icon;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: const TextField(
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      decoration: const BoxDecoration(),
+      child: TextField(
+        controller: controller,
         decoration: InputDecoration(
-          icon: Icon(Icons.email),
-          hintText: "Email",
+          border: InputBorder.none,
+          icon: Icon(icon),
+          hintText: hint,
         ),
       ),
     );
