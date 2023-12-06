2dee7c75b1e831790f466ec6d0e16237e7f64919
Morné le Roux <mornelerx@gmail.com>
2023-12-06 19:04:19 +0200

    added more boxes

--- lib/features/auth/presentation/screens/register_screen.dart
+++ lib/features/auth/presentation/screens/register_screen.dart
@@ -2,14 +2,35 @@ import 'package:fish_man/features/auth/presentation/widgets/auth_text_box.dart';
 import 'package:flutter/material.dart';
 
 class RegisterScreen extends StatelessWidget {
-  const RegisterScreen({super.key});
+  RegisterScreen({super.key});
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController password2Controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
           child: Column(
-        children: [AuthTextBox()],
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          AuthTextBox(
+            hint: "Email",
+            controller: emailController,
+            icon: Icons.email,
+          ),
+          AuthTextBox(
+            hint: "Password",
+            controller: passwordController,
+            icon: Icons.password,
+          ),
+          AuthTextBox(
+            hint: "Confirm Password",
+            controller: password2Controller,
+            icon: Icons.check_circle,
+          )
+        ],
       )),
     );
   }
