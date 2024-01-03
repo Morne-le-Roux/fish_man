0b0651a3be9ff70e05c694a38a7d9a4d7c27d96b
Morné le Roux <mornelerx@gmail.com>
2024-01-03 20:51:55 +0200

    changed to stream data and made supabase var private

--- lib/features/tanks_list/data/tanklist_database.dart
+++ lib/features/tanks_list/data/tanklist_database.dart
@@ -1,18 +1,26 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class TanklistDatabase {
-  final supabase = Supabase.instance.client;
+  final _supabase = Supabase.instance.client;
 
   getListOfTanks() async {
-    final data = await supabase.from("tankDatabase").select();
+    final data = await _supabase.from("tankDatabase").select();
     return data;
+  }
+
+  Stream taskStream({required ownerUID}) {
+    return _supabase
+        .from("tankDatabase")
+        .stream(primaryKey: ["id"]).eq("owner", ownerUID);
   }
 
   addTankToDatabase({
     required String tankName,
     required int tankSize,
+    required String owner,
   }) async {
-    await supabase.from("tankDatabase").insert({
+    await _supabase.from("tankDatabase").insert({
+      "owner": owner,
       "tank_name": tankName,
       "tank_size": tankSize,
     });
