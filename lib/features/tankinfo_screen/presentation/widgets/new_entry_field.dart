b7b0d8f6343d7ccbe6c7866b9e9887bad06439ae
Morné le Roux <mornelerx@gmail.com>
2024-01-08 21:29:05 +0200

    newEntryField init

---
+++ lib/features/tankinfo_screen/presentation/widgets/new_entry_field.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewEntryField extends StatelessWidget {
+  const NewEntryField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
