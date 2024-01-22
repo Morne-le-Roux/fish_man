0cf4308d8abb382bc6f30cd72881a8653eba11d1
Morné le Roux <mornelerx@gmail.com>
2024-01-22 21:35:59 +0200

    Push data to DB working

--- lib/features/tankinfo_screen/presentation/screens/new_entry_screen.dart
+++ lib/features/tankinfo_screen/presentation/screens/new_entry_screen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: non_constant_identifier_names
 
+import 'package:fish_man/features/tankinfo_screen/data/tank_entries_database.dart';
 import 'package:fish_man/features/tankinfo_screen/presentation/widgets/note_entry.dart';
 import 'package:fish_man/features/tankinfo_screen/presentation/widgets/subentry.dart';
 import 'package:flutter/material.dart';
@@ -16,9 +17,12 @@ class NewEntryScreen extends StatefulWidget {
 }
 
 class _NewEntryScreenState extends State<NewEntryScreen> {
+  //DB
+  final TankEntriesDatabase _tankDB = TankEntriesDatabase();
+
   //Controllers
   final TextEditingController _pHController = TextEditingController();
-  final TextEditingController _NH4Controller = TextEditingController();
+  final TextEditingController _NH3Controller = TextEditingController();
   final TextEditingController _NO2Controller = TextEditingController();
   final TextEditingController _NO3Controller = TextEditingController();
   final TextEditingController _GHController = TextEditingController();
@@ -28,6 +32,18 @@ class _NewEntryScreenState extends State<NewEntryScreen> {
 
   @override
   Widget build(BuildContext context) {
+    //LIST OF CONTROLLERS
+    List<TextEditingController> controllers = [
+      _GHController,
+      _KHController,
+      _NH3Controller,
+      _NO2Controller,
+      _NO3Controller,
+      _TDSController,
+      _pHController,
+      _noteController,
+    ];
+
     final List<Subentry> subEntryList = [
       Subentry(
         shortName: "pH",
@@ -35,9 +51,9 @@ class _NewEntryScreenState extends State<NewEntryScreen> {
         controller: _pHController,
       ),
       Subentry(
-        shortName: "NH4",
+        shortName: "NH3",
         fullName: "Ammonia",
-        controller: _NH4Controller,
+        controller: _NH3Controller,
       ),
       Subentry(
         shortName: "NO2",
@@ -70,9 +86,43 @@ class _NewEntryScreenState extends State<NewEntryScreen> {
       floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.check),
           onPressed: () {
-            Map data = {};
+            Map<String, dynamic> data = {"id": widget.tankID};
+
+            if (_pHController.text != "") {
+              data["pH"] = double.parse(_pHController.text);
+            }
+
+            if (_NH3Controller.text != "") {
+              data["NH3"] = double.parse(_NH3Controller.text);
+            }
+
+            if (_NO2Controller.text != "") {
+              data["NO2"] = double.parse(_NO2Controller.text);
+            }
+
+            if (_NO3Controller.text != "") {
+              data["NO3"] = double.parse(_NO3Controller.text);
+            }
+
+            if (_GHController.text != "") {
+              data["GH"] = double.parse(_GHController.text);
+            }
+
+            if (_KHController.text != "") {
+              data["KH"] = double.parse(_KHController.text);
+            }
+
+            if (_TDSController.text != "") {
+              data["TDS"] = double.parse(_TDSController.text);
+            }
+
+            if (_noteController.text != "") {
+              data["note"] = _noteController.text;
+            }
 
             print(data);
+
+            _tankDB.sendTankEntry(data: data);
           }),
       appBar: AppBar(
         title: Text("New Entry for ${widget.tankName}"),
